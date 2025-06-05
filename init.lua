de827e456f5083170f10ce1844e3e4b55008f5e5
Albert Mojir <albert.mojir@prisjakt.nu>
2025-06-05 23:58:49 +0200

    More plugins, more config

--- init.lua
+++ init.lua
@@ -71,7 +71,7 @@ require("lazy").setup({
           logfile = false,
           prompt_no_cr = false,
           autosave = {
-            current = false,
+            current = true,
             tmp = false,
             tmp_name = 'tmp',
             on_load = true,
@@ -92,7 +92,8 @@ require("lazy").setup({
         -- Load Telescope extension
         require('telescope').load_extension('possession')
       end
-    },{
+    },
+    {
       'lewis6991/gitsigns.nvim',
       config = function()
         require('gitsigns').setup({
@@ -148,7 +149,211 @@ require("lazy").setup({
           end
         })
       end
-    }
+    },
+    {
+      'akinsho/toggleterm.nvim',
+      version = "*",
+      config = function()
+        require("toggleterm").setup({
+          size = 15,
+          open_mapping = [[<leader>tt]],
+          hide_numbers = true,
+          shade_terminals = true,
+          shading_factor = 2,
+          start_in_insert = true,
+          insert_mappings = true,
+          persist_size = true,
+          direction = 'horizontal', -- 'vertical' | 'horizontal' | 'tab' | 'float'
+          close_on_exit = true,
+          shell = vim.o.shell,
+          float_opts = {
+            border = 'curved',
+            winblend = 0,
+            highlights = {
+              border = "Normal",
+              background = "Normal",
+            }
+          }
+        })
+
+        -- Custom terminal keymaps
+        function _G.set_terminal_keymaps()
+          local opts = {buffer = 0}
+          vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+          vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+          vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+          vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+          vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+        end
+
+        vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+        -- Additional keymaps for different terminal types
+        local Terminal = require('toggleterm.terminal').Terminal
+
+        -- Floating terminal
+        local float_term = Terminal:new({
+          direction = "float",
+          float_opts = {
+            border = "double",
+          },
+          hidden = true,
+        })
+
+        function _FLOAT_TOGGLE()
+          float_term:toggle()
+        end
+
+        vim.keymap.set('n', '<leader>tf', '<cmd>lua _FLOAT_TOGGLE()<CR>', { desc = 'Toggle floating terminal' })
+      end
+    },
+    {
+      'akinsho/bufferline.nvim',
+      version = "*",
+      dependencies = 'nvim-tree/nvim-web-devicons',
+      config = function()
+        require("bufferline").setup({
+          options = {
+            mode = "buffers", -- set to "tabs" to only show tabpages instead
+            numbers = "none", -- "none" | "ordinal" | "buffer_id" | "both"
+            close_command = "bdelete! %d",
+            right_mouse_command = "bdelete! %d",
+            left_mouse_command = "buffer %d",
+            middle_mouse_command = nil,
+            indicator = {
+              icon = '▎',
+              style = 'icon',
+            },
+            buffer_close_icon = '󰅖',
+            modified_icon = '●',
+            close_icon = '',
+            left_trunc_marker = '',
+            right_trunc_marker = '',
+            diagnostics = "nvim_lsp", -- Show LSP diagnostics
+            diagnostics_update_in_insert = false,
+            show_buffer_icons = true,
+            show_buffer_close_icons = true,
+            show_close_icon = true,
+            show_tab_indicators = true,
+            separator_style = "slant", -- "slant" | "thick" | "thin" | { 'any', 'any' }
+            enforce_regular_tabs = false,
+            always_show_bufferline = true,
+            hover = {
+              enabled = true,
+              delay = 200,
+              reveal = {'close'}
+            },
+            offsets = {
+              {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                text_align = "left",
+                separator = true
+              }
+            },
+          }
+        })
+      end
+    },
+    {
+      'akinsho/bufferline.nvim',
+      version = "*",
+      dependencies = 'nvim-tree/nvim-web-devicons',
+      config = function()
+        require("bufferline").setup({
+          options = {
+            mode = "buffers",
+            numbers = "none",
+            close_command = "bdelete! %d",
+            right_mouse_command = "bdelete! %d",
+            left_mouse_command = "buffer %d",
+            middle_mouse_command = nil,
+            indicator = {
+              icon = '▎',
+              style = 'icon',
+            },
+            buffer_close_icon = '󰅖',
+            modified_icon = '●',
+            close_icon = '',
+            left_trunc_marker = '',
+            right_trunc_marker = '',
+            diagnostics = "nvim_lsp",
+            show_buffer_icons = true,
+            show_buffer_close_icons = true,
+            show_close_icon = true,
+            show_tab_indicators = true,
+            separator_style = "slant",
+            always_show_bufferline = true,
+            hover = {
+              enabled = true,
+              delay = 200,
+              reveal = {'close'}
+            },
+            -- Filter out directory buffers and nvim-tree
+            custom_filter = function(buf_number, buf_numbers)
+              local buf_name = vim.api.nvim_buf_get_name(buf_number)
+              local buf_ft = vim.bo[buf_number].filetype
+
+              -- Hide directory buffers and nvim-tree
+              if buf_ft == "NvimTree" then
+                return false
+              end
+
+              -- Hide directory buffers (they usually end with / or are directories)
+              if vim.fn.isdirectory(buf_name) == 1 then
+                return false
+              end
+
+              -- Hide unnamed buffers that are directories
+              if buf_name == "" or buf_name:match("/$") then
+                return false
+              end
+
+              return true
+            end,
+            offsets = {
+              {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                text_align = "left",
+                separator = true
+              }
+            },
+          }
+        })
+      end
+    },
+    {
+      'mbbill/undotree',
+      config = function()
+        -- Configure undotree
+        vim.g.undotree_WindowLayout = 2
+        vim.g.undotree_ShortIndicators = 1
+        vim.g.undotree_SetFocusWhenToggle = 1
+
+        -- Keymap to toggle undotree
+        vim.keymap.set('n', '<leader>u', ':UndotreeToggle<CR>', { desc = 'Toggle undo tree' })
+      end
+    },
+    -- Better colorscheme
+    {
+      'catppuccin/nvim',
+      name = 'catppuccin',
+      priority = 1000,
+      config = function()
+        require('catppuccin').setup({
+          flavour = 'mocha', -- latte, frappe, macchiato, mocha
+          transparent_background = false,
+          integrations = {
+            nvimtree = true,
+            telescope = true,
+            gitsigns = true,
+            bufferline = true,
+          },
+        })
+        vim.cmd.colorscheme('catppuccin')
+      end
+    },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
@@ -157,17 +362,49 @@ require("lazy").setup({
   checker = { enabled = true },
 })
 
--- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  filters = {
+    dotfiles = false,  -- Show dotfiles like .gitignore
+    git_clean = false, -- Show files ignored by git
+    no_buffer = false,
+    custom = {
+      -- Remove lazy-lock.json from the default ignore list
+      -- You can add other files you want to hide here
+    },
+  },
+  git = {
+    enable = true,
+    ignore = false,  -- Show git ignored files
+    show_on_dirs = true,
+    show_on_open_dirs = true,
+    timeout = 400,
+  },
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+})
 
 
-vim.api.nvim_set_keymap('n', '<Leader>t', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
--- Buffer navigation
-vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { noremap = true })
-vim.keymap.set('n', '<leader>bp', ':bprevious<CR>', { noremap = true })
-vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>te', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+-- Buffer/tab navigation with bufferline
+vim.keymap.set('n', '<leader>bn', ':BufferLineCycleNext<CR>', { noremap = true, desc = 'Next buffer' })
+vim.keymap.set('n', '<leader>bp', ':BufferLineCyclePrev<CR>', { noremap = true, desc = 'Previous buffer' })
+vim.keymap.set('n', '<leader>bd', ':BufferLinePickClose<CR>', { noremap = true, desc = 'Pick buffer to close' })
+vim.keymap.set('n', '<leader>bc', ':BufferLineCloseRight<CR>:BufferLineCloseLeft<CR>', { noremap = true, desc = 'Close all but current' })
 vim.keymap.set('n', '<leader>ls', ':ls<CR>', { noremap = true })
 
+-- Quick buffer switching with numbers
+vim.keymap.set('n', '<leader>1', '<Cmd>BufferLineGoToBuffer 1<CR>', { desc = 'Go to buffer 1' })
+vim.keymap.set('n', '<leader>2', '<Cmd>BufferLineGoToBuffer 2<CR>', { desc = 'Go to buffer 2' })
+vim.keymap.set('n', '<leader>3', '<Cmd>BufferLineGoToBuffer 3<CR>', { desc = 'Go to buffer 3' })
+vim.keymap.set('n', '<leader>4', '<Cmd>BufferLineGoToBuffer 4<CR>', { desc = 'Go to buffer 4' })
+vim.keymap.set('n', '<leader>5', '<Cmd>BufferLineGoToBuffer 5<CR>', { desc = 'Go to buffer 5' })
 
 -- FuzzyFind (telescope)
 local builtin = require('telescope.builtin')
@@ -183,6 +420,11 @@ vim.keymap.set('n', '<leader>sf', '<cmd>Telescope possession list<CR>', { desc =
 vim.keymap.set('n', '<leader>sd', ':PossessionDelete<CR>', { desc = 'Delete session' })
 vim.keymap.set('n', '<leader>sr', ':PossessionRename<CR>', { desc = 'Rename session' })
 
+-- Buffer navigation with Ctrl+Tab and Ctrl+Shift+Tab
+vim.keymap.set('n', '<M-Tab>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true, desc = 'Next buffer' })
+vim.keymap.set('n', '<M-S-Tab>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true, desc = 'Previous buffer' })
+
+
 -- Use spaces instead of tabs
 vim.opt.expandtab = true      -- Convert tabs to spaces
 vim.opt.tabstop = 2           -- Number of spaces a tab counts for
@@ -192,3 +434,43 @@ vim.opt.wrap = false -- Disable line wrapping
 vim.opt.mouse = 'a'
 -- vim.opt.clipboard = 'unnamedplus'
 vim.opt.hidden = true -- Allow switching buffers without saving
+
+-- Disable swapfiles
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+
+-- Show line numbers
+vim.opt.number = true
+
+-- Simple keymap to quit all
+vim.keymap.set('n', '<leader>qa', ':qall<CR>', { desc = 'Quit all' })
+
+-- Window navigation with Option+Arrow keys (including terminal mode)
+local nav_maps = {
+  ['<M-Left>'] = '<C-w>h',
+  ['<M-Right>'] = '<C-w>l', 
+  ['<M-Up>'] = '<C-w>k',
+  ['<M-Down>'] = '<C-w>j'
+}
+
+for key, cmd in pairs(nav_maps) do
+  vim.keymap.set('n', key, cmd, { desc = 'Navigate windows' })
+  vim.keymap.set('t', key, '<C-\\><C-n>' .. cmd, { desc = 'Navigate from terminal' })
+end
+
+-- Auto-save buffers
+vim.api.nvim_create_autocmd({"FocusLost", "BufLeave", "CursorHold", "CursorHoldI"}, {
+  pattern = "*",
+  callback = function()
+    if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+      vim.api.nvim_command('silent! write')
+    end
+  end,
+})
+
+-- Enable persistent undo (add this with your other vim.opt settings)
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.stdpath('data') .. '/undo'
+vim.opt.undolevels = 10000
+vim.opt.undoreload = 10000
