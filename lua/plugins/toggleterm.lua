370e8153a29f225414d07190e45fa5fd63162934
Albert Mojir <albert.mojir@prisjakt.nu>
2025-06-17 23:09:43 +0200

    Updated toggle terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -34,20 +34,12 @@ return {
       function _G.set_terminal_keymaps()
         local opts = { buffer = 0, noremap = true, silent = true }
         vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-        vim.keymap.set("t", "<C-c>", [[<C-\><C-n>]], opts)
         vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
         vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
         vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
         vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
         vim.keymap.set("n", "i", "i", { buffer = 0 })
         vim.keymap.set("n", "a", "a", { buffer = 0 })
-        vim.keymap.set("n", "q", function()
-          local bufnr = vim.api.nvim_get_current_buf()
-          if vim.api.nvim_buf_is_valid(bufnr) then
-            vim.bo[bufnr].modified = false
-          end
-          vim.cmd("close")
-        end, { buffer = 0, desc = "Close terminal" })
       end
 
       vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
@@ -56,19 +48,21 @@ return {
 
       local float_term = Terminal:new({
         direction = "float",
-        float_opts = {
-          border = "double",
-        },
         hidden = true,
       })
 
       function _FLOAT_TOGGLE()
         float_term:toggle()
+        vim.defer_fn(function()
+          if vim.bo.buftype == "terminal" then
+            vim.cmd("startinsert")
+          end
+        end, 50)
       end
 
-      vim.keymap.set("n", "<leader>tf", "<cmd>lua _FLOAT_TOGGLE()<CR>", { desc = "Toggle floating terminal" })
+      vim.keymap.set("n", "<leader>tt", "<cmd>lua _FLOAT_TOGGLE()<CR>", { desc = "Toggle floating terminal" })
 
-      vim.keymap.set("n", "<leader>tt", function()
+      vim.keymap.set("n", "<leader>tT", function()
         vim.cmd("ToggleTerm direction=horizontal")
         vim.defer_fn(function()
           if vim.bo.buftype == "terminal" then
@@ -76,14 +70,6 @@ return {
           end
         end, 50)
       end, { desc = "Toggle horizontal terminal and enter insert mode" })
-
-      vim.keymap.set(
-        "n",
-        "<leader>th",
-        "<cmd>ToggleTerm direction=horizontal<CR>",
-        { desc = "Toggle horizontal terminal" }
-      )
-      vim.keymap.set("n", "<leader>tv", "<cmd>ToggleTerm direction=vertical<CR>", { desc = "Toggle vertical terminal" })
     end,
   },
 }
