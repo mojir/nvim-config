25ef86e2b7da5681db8fde798fc9741f5466e758
Albert Mojir <albert.mojir@prisjakt.nu>
2025-06-18 12:52:23 +0200

    Keymaps for git
    Signed-off-by: Albert Mojir <albert.mojir@prisjakt.nu>

--- lua/plugins/editor/diffview.lua
+++ lua/plugins/editor/diffview.lua
@@ -13,11 +13,25 @@ return {
       })
 
       -- Essential keymaps
-      vim.keymap.set("n", "<leader>dvo", "<cmd>DiffviewOpen<cr>", { desc = "Open diffview" })
-      vim.keymap.set("n", "<leader>dvc", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" })
-      vim.keymap.set("n", "<leader>dvh", "<cmd>DiffviewFileHistory<cr>", { desc = "File history" })
-      vim.keymap.set("n", "<leader>dvH", "<cmd>DiffviewFileHistory %<cr>", { desc = "Current file history" })
-      vim.keymap.set("n", "<leader>dvr", "<cmd>DiffviewRefresh<cr>", { desc = "Refresh diffview" })
+      vim.keymap.set("n", "<leader>gh", "<cmd>DiffviewFileHistory<cr>", { desc = "File history" })
+      vim.keymap.set("n", "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", { desc = "Current file history" })
+      vim.keymap.set("n", "<leader>gr", "<cmd>DiffviewRefresh<cr>", { desc = "Refresh diffview" })
+      -- Toggle diffview
+      vim.keymap.set("n", "<leader>gd", function()
+        local lib = require("diffview.lib")
+        local view = lib.get_current_view()
+        if view then
+          -- Force close any diffview
+          vim.cmd("DiffviewClose")
+          -- If that didn't work, try tabclose
+          if lib.get_current_view() then
+            vim.cmd("tabclose")
+          end
+        else
+          -- Open with file panel (same as default DiffviewOpen)
+          vim.cmd("DiffviewOpen")
+        end
+      end, { desc = "Toggle diffview" })
     end,
   },
 }
